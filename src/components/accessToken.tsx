02d954374ae7a72fb45d4f5077eb5fddcdca619c
Ahmad hilmi <ahmadhilmi420>
2025-07-28 01:23:12 +0700

    update run build

--- src/components/accessToken.tsx
+++ src/components/accessToken.tsx
@@ -1,10 +1,10 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 export default function Component() {
-  const { data } = useSession()
-  if (!data) {
-    return <div>No session data available</div>
-  }
+	const { data } = useSession();
+	if (!data) {
+		return <div>No session data available</div>;
+	}
 
-  const { accessToken } = data as { accessToken?: string }
-  return <div>Access Token: {accessToken || "Not available"}</div>
+	const { accessToken } = data as { accessToken?: string };
+	return <div>Access Token: {accessToken || "Not available"}</div>;
 }
